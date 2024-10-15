a827bc357a2e11d0699b86fb62f6f945eb2c55d9
KIMHYEJIN <0411khj@naver.com>
2024-10-16 03:00:27 +0900

    init commit

---
+++ src/main/java/com/sparta/schedulemanagementjpa/ScheduleManagementJpaApplication.java
@@ -0,0 +1,13 @@
+package com.sparta.schedulemanagementjpa;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ScheduleManagementJpaApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ScheduleManagementJpaApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/sparta/schedulemanagementjpa/ScheduleManagementJpaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sparta.schedulemanagementjpa;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ScheduleManagementJpaApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
