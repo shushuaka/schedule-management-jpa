4b23f445bf43da9b72fe342b4a18221fb71ffd7d
KIMHYEJIN <0411khj@naver.com>
2024-10-31 12:20:20 +0900

    feat: Add PasswordEncoder for password encryption using BCrypt
    - BCrypt를 사용한 비밀번호 암호화 기능 추가

---
+++ src/main/java/com/sparta/schedulemanagementjpa/config/PasswordEncoder.java
@@ -0,0 +1,19 @@
+package com.sparta.schedulemanagementjpa.config;
+
+import at.favre.lib.crypto.bcrypt.BCrypt;
+import org.springframework.stereotype.Component;
+
+@Component
+public class PasswordEncoder {
+
+    // 비밀번호 암호화
+    public String encode(String rawPassword) {
+        return BCrypt.withDefaults().hashToString(BCrypt.MIN_COST, rawPassword.toCharArray());
+    }
+
+    // 비밀번호 비교
+    public boolean matches(String rawPassword, String encodedPassword) {
+        BCrypt.Result result = BCrypt.verifyer().verify(rawPassword.toCharArray(), encodedPassword);
+        return result.verified;
+    }
+}
